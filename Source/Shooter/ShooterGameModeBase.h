5a4d7633b55ea4abd4e5e0994e2be1a17d225e50
DESKTOP-1TM2UJN\vercl <verclocker1@gmail.com>
2021-08-24 16:57:27 +0400

    Initial commit

---
+++ Source/Shooter/ShooterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTER_API AShooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
