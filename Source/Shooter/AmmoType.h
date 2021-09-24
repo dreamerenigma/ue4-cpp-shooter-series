b66a7d9904dfb53100f210a111f1c5f955a0c38a
Andrey <Hitmanki@yandex.ru>
2021-09-25 00:53:48 +0400

    Added folders and files

---
+++ Source/Shooter/AmmoType.h
@@ -0,0 +1,10 @@
+#pragma once
+
+UENUM(BlueprintType)
+enum class EAmmoType : uint8
+{
+	EAT_9mm UMETA(DisplayName = "9mm"),
+	EAT_AR UMETA(DisplayName = "Assault Rifle"),
+
+	EAT_MAX UMETA(DisplayName = "DefaultMAX")
+};
---
+++ Source/Shooter/EnemyController.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "Enemy.h"
+#include "BehaviorTree/BehaviorTree.h"
+
+AEnemyController::AEnemyController()
+{
+	BlackboardComponent = CreateDefaultSubobject<UBlackboardComponent >(TEXT("BlackboardComponent"));
+	check(BlackboardComponent);
+
+	BehaviorTreeComponent = CreateDefaultSubobject<UBehaviorTreeComponent >(TEXT("BehaviorTreeComponent"));
+	check(BehaviorTreeComponent);
+}
+
+void AEnemyController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+
+	if (InPawn == nullptr) return;
+
+	AEnemy* Enemy = Cast<AEnemy>(InPawn);
+	if (Enemy)
+	{
+		if (Enemy->GetBehaviorTree())
+		{
+			BlackboardComponent->InitializeBlackboard(*(Enemy->GetBehaviorTree()->BlackboardAsset));
+		}
+	}
+}
